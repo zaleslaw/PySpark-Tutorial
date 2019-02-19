27c99adadf3f984c19cda97422e63c554d8b026d
zaleslaw <zaleslaw.sin@gmail.com>
2019-02-19 14:58:25 +0300

    Added examples for Spark-Standalone cluster

---
+++ 6_Spark_Standalone/Step_1.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+import os
+
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
+
+    spark = SparkSession \
+        .builder \
+        .master("spark://172.18.0.2:7077") \
+        .appName("RDD_Intro") \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    # SAMPLE-1: Make dataset based on range and extract RDD from it
+
+    ds = spark.range(10000000)
+    print("Count %i" % ds.count())
+    print("Count in rdd %i" % ds.rdd.count())
+
+    spark.stop()
